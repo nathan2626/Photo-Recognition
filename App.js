ba84d4346d12d5637e31bdcc05014c7a446187ab
nathan2626 <61419599+nathan2626@users.noreply.github.com>
2022-06-10 17:10:36 +0200

    Add modification

--- App.js
+++ App.js
@@ -1,23 +1,65 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   Text,
+  Alert,
   NativeModules,
   Button,
 } from 'react-native';
-import * as ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
 const App = () => {
   const [label, setLabel] = useState();
+  const [response, setResponse] = useState(null);
 
-  const handleOnPress = () => {
-    ImagePicker.showImagePicker({}, async response => {
+  useEffect(() => {
+    async function getLabel(uri){
+      const detectedLabel = await NativeModules.Detector.detect(uri);
+      setLabel(detectedLabel);
+    }
+    if(response){
+      try{
+        //const imageCapturee = response.assets[0].uri.replace('image: file://')
+        const imageCapturee = response.assets[0].uri
+        console.log('image:', imageCapturee)
+        getLabel(imageCapturee)
+      }catch(err){
+        console.log('err:', err)
+      }
+      
+    }
+  }, [response]); 
+
+  const handleOnPress = React.useCallback((type) => {
+    const options = {
+      title: 'Select Image',
+      type: 'library',
+      options: {
+        maxHeight: 299,
+        maxWidth: 299,
+        selectionLimit: 0,
+        mediaType: 'photo',
+        includeBase64: true,
+      },
+    }
+
+    launchImageLibrary(options, setResponse); 
+  }, [])
+  
+
+
+  const handleOnPress1 = async () => {
+    /*launchImageLibrary({}, async response => {
       if (!response.didCancel) {
         const detectedLabel = await NativeModules.Detector.detect(response.uri);
         setLabel(detectedLabel);
       }
-    });
+    });*/
+    //launchImageLibrary(options?, callback)
+
+    // You can also use as a promise without 'callback':
+    //const result = await launchImageLibrary(options?);
   };
 
   return (
